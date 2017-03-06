eaaf6722ac52c24b58c6473e6b8802019aa74043
snagasawa <whitecrane8920@gmail.com>
2017-03-07 08:16:49 +0900

    Show message items

--- app/assets/javascripts/components/message_box.js.jsx
+++ app/assets/javascripts/components/message_box.js.jsx
@@ -1,8 +1,24 @@
 var MessageBox = React.createClass({
+  getInitialState: function() {
+    return {
+      messages: [
+        { id: 1, user: 'Tom', text: 'Good morning' },
+        { id: 2, user: 'John', text: 'Good afternoon' },
+        { id: 3, user: 'Emily', text: 'Good evening' }
+      ]
+    };
+  },
+
   render: function() {
+    var messageItems = this.state.messages.map(function(message) {
+      return (
+        <MessageItem key={message.id} message={message}/>
+      );
+    });
+
     return (
       <div className="messageBox">
-        This is message box.
+        {messageItems}
       </div>
     );
   }
